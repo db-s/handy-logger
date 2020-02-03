4f6996462c0af7993785f4b39f6a57a582f263da
db-s <sinha089@gmail.com>
2020-02-03 20:02:28 +0530

    Modified test command, added new test case

--- src/__tests__/logger.test.ts
+++ src/__tests__/logger.test.ts
@@ -1,8 +1,20 @@
-import { easyLogger, EasyLogger } from '../logger';
+import { EasyLogger } from '../logger';
 import { defaultLoggerConfig } from '../config';
+import * as winston from 'winston';
+import { Logger } from 'winston';
+
+let easyLogger: EasyLogger;
+
+beforeEach(() => {
+  easyLogger = new EasyLogger();
+});
 
 describe('Logger Test Cases', () => {
-  test('logger level', () => {
+  test('create logger instance', () => {
+    expect(easyLogger).toBeInstanceOf(EasyLogger);
+  });
+
+  test('get logger config', () => {
     expect(easyLogger.getConfig()).toMatchObject(defaultLoggerConfig);
   });
 });
