e866f09f29244be827dfa12a197fb89991c81e5f
db-s <sinha089@gmail.com>
2020-04-03 12:18:05 +0530

    Fixed build issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import * as winston from 'winston';
+
 export * from './interfaces';
 export * from './enums';
 export { HandyLogger, HandyLoggerBase } from './logger';
-
-import * as winston from 'winston';
 export { winston };
